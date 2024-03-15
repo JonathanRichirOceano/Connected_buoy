31a8d79097917bc072733b6ded6b308f6b11dc87
Jonathan Richir <jonathan.richir@gmail.com>
2024-03-15 15:04:34 +0000

    upload_20240315

--- ts_24/Analyses_2024.R
+++ ts_24/Analyses_2024.R
@@ -33,7 +33,7 @@ table(duplicated(df$Datetime))
 # àpad de mars 2023 qd la sonde est revenue d'entretien: "les paramètres Chloro-A sont calibrés en utilisant des ppb de Rhodamine WT.400ppb de rhodamine WT équivalent à 40µg/l de chlorophylle." => facteur 10 de correction.
 df$Fluorescence <- df$Fluorescence/10
 
-saveRDS(df, "ts_24/raw.data_2024.rds")
+saveRDS(df, "ts_24/Data_2024/raw.data_2024.rds")
 
 go1 <- melt(df[, c(2:6,8)], id.vars= "Datetime")
 go1$value <- as.numeric(go1$value)
@@ -75,7 +75,7 @@ show(ind)
 show(z)
 df[c(sort(unique(c(z,ind)))),] 
 
-# Anlayse data via ploting when difftime != 15 min
+# Analyse data via ploting when difftime != 15 min
 
 buoy.plot <- function(var., ylab.) {
   df.plot <- df[, c("Datetime", var.)]
@@ -95,33 +95,56 @@ buoy.plot <- function(var., ylab.) {
 buoy.plot(var. = "Temperature", ylab. = "Température (°C)")
 (p.temp.raw <- p.)
 rm(p.)
-#ggsave("Figures/p.temp.raw.png", width = 5, height = 2.5, dpi = 300)
 
 buoy.plot(var. = "Salinite", ylab. = "Salinité")
 (p.sal.raw <- p.)
 rm(p.)
-#ggsave("Figures/p.sal.raw.png", width = 5, height = 2.5, dpi = 300)
 
 buoy.plot(var. = "Oxygene", ylab. = "Oxygène dissous (mg/L)")
 (p.oxy.raw <- p.)
 rm(p.)
-#ggsave("Figures/p.oxy.raw.png", width = 5, height = 2.5, dpi = 300)
 
 buoy.plot(var. = "pH", ylab. = "pH")
 (p.pH.raw <- p.)
 rm(p.)
-#ggsave("Figures/p.pH.raw.png", width = 5, height = 2.5, dpi = 300)
 
 buoy.plot(var. = "Fluorescence", ylab. = "Chla (µg/L)")
 (p.fluo.raw <- p.)
 rm(p.)
-#ggsave("Figures/p.fluo.raw.png", width = 5, height = 2.5, dpi = 300)
+
+# Apply on 2024 raw data some corrections following a process similar to that of 2022-2023 data.
+# This section will have to be updated according to data sent by Kevin
+# either we remove a date, i.e. all parameter data, or we just replace by NA incoherent values for some parameters 
+
+bluelines <- data.frame(Datetime = NA)
+
+df.raw <- df
+df.clean <- df
+
+filter(df.clean, Salinite >= 20) -> df.clean
+bluelines <- bind_rows(bluelines, unique(filter(df.raw, Salinite < 20)["Datetime"]))
+
+#filter(df.clean, pH >= 8.20) -> df.clean
+#bluelines <- bind_rows(bluelines, unique(filter(df.raw, pH < 8.20)["Datetime"]))
+#filter(df.clean, pH <= 8.35) -> df.clean
+#bluelines <- bind_rows(bluelines, unique(filter(df.raw, pH > 8.35)["Datetime"]))
+df.clean$pH <- ifelse(df.clean$pH < 8.20, NA, df.clean$pH)
+df.clean$pH <- ifelse(df.clean$pH > 8.35, NA, df.clean$pH)
+
+filter(df.clean, Fluorescence <= 40) -> df.clean
+bluelines <- bind_rows(bluelines, unique(filter(df.raw, Fluorescence > 40)["Datetime"]))
+
+# ...
+# add here above any suppl. filter
+
+(bluelines <- unique(na.omit(bluelines)))
+
 
 filter(df, difftime.min > 60)
 z <- which(df$difftime.min > 60)
 ind <- unique(unlist(lapply(z, function(x){
-  g <- pmax(x - 12, 1)
-  g : (x + 11)
+  g <- pmax(x - 4, 1)
+  g : (x + 3)
 })))
 length(ind)
 show(ind)
@@ -129,26 +152,21 @@ show(z)
 (df.del <- df[c(sort(unique(c(z,ind)))),]) 
 
 library(dplyr)
-df.clean <- setdiff(df, df.del)
-df.raw <- df
+df.clean <- setdiff(df.clean, df.del)
 #rm(df.del)
-
-# Apply on 2024 raw data some corrections following a process similar to that of 2022-2023 data.
-# This section will have to be updated according to data sent by Kevin
-# either we remove a date, i.e. all parameter data, or we just replace by NA incoherent values for some parameters ...
 
 setdiff(df.raw, df.clean)
 ind <- unique(unlist(lapply(z, function(x){
   g <- pmax(x - 1, 1)
 })))
 df[c(sort(unique(c(z,ind)))),]$date 
-bluelines.holes <- df[c(sort(unique(c(z,ind)))),]$Datetime 
+bluelines <- bind_rows(bluelines, df[c(sort(unique(c(z,ind)))),]["Datetime"]) 
 
 ts_22to23.raw <- readRDS("ts_22_23/full.raw.data.ts_Lovina-Jojo.rds")
 ts_22to23.clean <- readRDS("ts_22_23/full.clean.data.ts_Lovina-Jojo.rds")
 
-ts_22to24.raw <- rbind(ts_22to23.raw, df.raw[,1:10])
-ts_22to24.clean <- rbind(ts_22to23.clean, df.clean[,1:10])
+ts_full.raw <- rbind(ts_22to23.raw, df.raw[,1:10])
+ts_full.clean <- rbind(ts_22to23.clean, df.clean[,1:10])
 
 #rm(ts_22to23.raw, ts_22to23.clean)
 
@@ -156,7 +174,7 @@ ts_22to24.clean <- rbind(ts_22to23.clean, df.clean[,1:10])
 
 # plot raw data
 
-go1 <- melt(ts_22to24.raw[, c(2:6,8)], id.vars= "Datetime")
+go1 <- melt(ts_full.raw[, c(2:6,8)], id.vars= "Datetime")
 go1$value <- as.numeric(go1$value)
 
 ggplot(go1, aes(Datetime, value)) +
@@ -215,43 +233,63 @@ buoy.plot <- function(df., var., ylab.) {
     # 2024 and more
     geom_vline(aes(xintercept = last(ts_22to23.raw$Datetime)), col = "blue", alpha = 0.4) + 
     geom_vline(aes(xintercept = first(df.raw$Datetime)), col = "blue", alpha = 0.4) + 
+    geom_vline(data = bluelines, aes(xintercept = Datetime), col = "blue", alpha = 0.4) +
     theme_bw() +
     theme(axis.text.x = element_text(angle = 45, hjust=1))
-  # 2024 and more
-  for(i in 1:length(bluelines.holes)){ 
-    p. <<- p. + geom_vline(aes(xintercept = bluelines.holes[i]), col = "blue", alpha = 0.4)
-  }
-  #return(p.)
 }
 
-buoy.plot(df. = ts_22to24.raw, var. = "Temperature", ylab. = "Température (°C)")
+# first, year currently analysed afeter filtering for zoom purpose
+
+buoy.plot(df. = df.raw, var. = "Temperature", ylab. = "Température (°C)")
+(p.temp.raw <- p.)
+rm(p.)
+
+buoy.plot(df. = df.raw, var. = "Salinite", ylab. = "Salinité")
+(p.sal.raw <- p.)
+rm(p.)
+
+buoy.plot(df. = df.raw, var. = "Oxygene", ylab. = "Oxygène dissous (mg/L)")
+(p.oxy.raw <- p.)
+rm(p.)
+
+buoy.plot(df. = df.raw, var. = "pH", ylab. = "pH")
+(p.pH.raw <- p.)
+rm(p.)
+
+buoy.plot(df. = df.raw, var. = "Fluorescence", ylab. = "Chla (µg/L)")
+(p.fluo.raw <- p.)
+rm(p.)
+
+# second, full ts
+
+buoy.plot(df. = ts_full.raw, var. = "Temperature", ylab. = "Température (°C)")
 (p.temp.raw <- p.)
 rm(p.)
 ggsave("ts_24/Figs./raw/p.temp.raw.png", width = 5, height = 2.5, dpi = 300)
 
-buoy.plot(df. = ts_22to24.raw, var. = "Salinite", ylab. = "Salinité")
+buoy.plot(df. = ts_full.raw, var. = "Salinite", ylab. = "Salinité")
 (p.sal.raw <- p.)
 rm(p.)
 ggsave("ts_24/Figs./raw/p.sal.raw.png", width = 5, height = 2.5, dpi = 300)
 
-buoy.plot(df. = ts_22to24.raw, var. = "Oxygene", ylab. = "Oxygène dissous (mg/L)")
+buoy.plot(df. = ts_full.raw, var. = "Oxygene", ylab. = "Oxygène dissous (mg/L)")
 (p.oxy.raw <- p.)
 rm(p.)
 ggsave("ts_24/Figs./raw/p.oxy.raw.png", width = 5, height = 2.5, dpi = 300)
 
-buoy.plot(df. = ts_22to24.raw, var. = "pH", ylab. = "pH")
+buoy.plot(df. = ts_full.raw, var. = "pH", ylab. = "pH")
 (p.pH.raw <- p.)
 rm(p.)
 ggsave("ts_24/Figs./raw/p.pH.raw.png", width = 5, height = 2.5, dpi = 300)
 
-buoy.plot(df. = ts_22to24.raw, var. = "Fluorescence", ylab. = "Chla (µg/L)")
+buoy.plot(df. = ts_full.raw, var. = "Fluorescence", ylab. = "Chla (µg/L)")
 (p.fluo.raw <- p.)
 rm(p.)
 ggsave("ts_24/Figs./raw/p.fluo.raw.png", width = 5, height = 2.5, dpi = 300)
 
 # plot clean data
 
-go1 <- melt(ts_22to24.clean[, c(2:6,8)], id.vars= "Datetime")
+go1 <- melt(ts_full.clean[, c(2:6,8)], id.vars= "Datetime")
 go1$value <- as.numeric(go1$value)
 
 ggplot(go1, aes(Datetime, value)) +
@@ -284,65 +322,65 @@ buoy.plot.clean <- function(df., var., ylab.) {
     theme(axis.text.x = element_text(angle = 45, hjust=1))
 }
 
-buoy.plot.clean(df. = ts_22to24.clean, var. = "Temperature", ylab. = "Température (°C)")
+buoy.plot.clean(df. = ts_full.clean, var. = "Temperature", ylab. = "Température (°C)")
 (p.temp.clean <- p.)
 rm(p.)
 ggsave("ts_24/Figs/clean/p.temp.clean.png", width = 5, height = 2.5, dpi = 300)
 
-buoy.plot.clean(df. = ts_22to24.clean, var. = "Salinite", ylab. = "Salinité")
+buoy.plot.clean(df. = ts_full.clean, var. = "Salinite", ylab. = "Salinité")
 (p.sal.clean <- p.)
 rm(p.)
 ggsave("ts_24/Figs/clean/p.sal.clean.png", width = 5, height = 2.5, dpi = 300)
 
-buoy.plot.clean(df. = ts_22to24.clean, var. = "Oxygene", ylab. = "Oxygène dissous (mg/L)")
+buoy.plot.clean(df. = ts_full.clean, var. = "Oxygene", ylab. = "Oxygène dissous (mg/L)")
 (p.oxy.clean <- p.)
 rm(p.)
 ggsave("ts_24/Figs/clean/p.oxy.clean.png", width = 5, height = 2.5, dpi = 300)
 
-buoy.plot.clean(df. = ts_22to24.clean, var. = "pH", ylab. = "pH")
+buoy.plot.clean(df. = ts_full.clean, var. = "pH", ylab. = "pH")
 (p.pH.clean <- p.)
 rm(p.)
 ggsave("ts_24/Figs/clean/p.pH.clean.png", width = 5, height = 2.5, dpi = 300)
 
-buoy.plot.clean(df. = ts_22to24.clean, var. = "Fluorescence", ylab. = "Chla (µg/L)")
+buoy.plot.clean(df. = ts_full.clean, var. = "Fluorescence", ylab. = "Chla (µg/L)")
 (p.fluo.clean <- p.)
 rm(p.)
 ggsave("ts_24/Figs/clean/p.fluo.clean.png", width = 5, height = 2.5, dpi = 300)
 
 # plot des raw data et des data removed
 
-diff.ts <- setdiff(ts_22to24.raw,ts_22to24.clean)
-diff.temp.ts <- setdiff(ts_22to24.raw[,c(2,8:10)],ts_22to24.clean[,c(2,8:10)])
-diff.sal.ts <- setdiff(ts_22to24.raw[,c(3,8:10)],ts_22to24.clean[,c(3,8:10)])
-diff.O2.ts <- setdiff(ts_22to24.raw[,c(4,8:10)],ts_22to24.clean[,c(4,8:10)])
-diff.pH.ts <- setdiff(ts_22to24.raw[,c(5,8:10)],ts_22to24.clean[,c(5,8:10)])
-diff.fluo.ts <- setdiff(ts_22to24.raw[,c(6,8:10)],ts_22to24.clean[,c(6,8:10)])
+diff.ts <- setdiff(ts_full.raw,ts_full.clean)
+diff.temp.ts <- setdiff(ts_full.raw[,c(2,8:10)],ts_full.clean[,c(2,8:10)])
+diff.sal.ts <- setdiff(ts_full.raw[,c(3,8:10)],ts_full.clean[,c(3,8:10)])
+diff.O2.ts <- setdiff(ts_full.raw[,c(4,8:10)],ts_full.clean[,c(4,8:10)])
+diff.pH.ts <- setdiff(ts_full.raw[,c(5,8:10)],ts_full.clean[,c(5,8:10)])
+diff.fluo.ts <- setdiff(ts_full.raw[,c(6,8:10)],ts_full.clean[,c(6,8:10)])
 
-p <- ggplot(ts_22to24.raw, aes(x=Datetime, y=Temperature)) + 
+p <- ggplot(ts_full.raw, aes(x=Datetime, y=Temperature)) + 
   geom_point() +
   xlab("") +
   ylab("Température (°C)")
 p.temp <- p + geom_point(data=diff.temp.ts,colour="red")
 
-p <- ggplot(ts_22to24.raw, aes(x=Datetime, y=Salinite)) + 
+p <- ggplot(ts_full.raw, aes(x=Datetime, y=Salinite)) + 
   geom_point() +
   xlab("") +
   ylab("Salinité")
 p.sal <- p + geom_point(data=diff.sal.ts,colour="red")
 
-p <- ggplot(ts_22to24.raw, aes(x=Datetime, y=Oxygene)) + 
+p <- ggplot(ts_full.raw, aes(x=Datetime, y=Oxygene)) + 
   geom_point() +
   xlab("") +
   ylab("Oxygène dissous (mg/L)")
 p.O2 <- p + geom_point(data=diff.O2.ts,colour="red")
 
-p <- ggplot(ts_22to24.raw, aes(x=Datetime, y=pH)) + 
+p <- ggplot(ts_full.raw, aes(x=Datetime, y=pH)) + 
   geom_point() +
   xlab("") +
-  ylab("pH (pH)")
+  ylab("pH")
 p.pH <- p + geom_point(data=diff.pH.ts,colour="red")
 
-p <- ggplot(ts_22to24.raw, aes(x=Datetime, y=Fluorescence)) + 
+p <- ggplot(ts_full.raw, aes(x=Datetime, y=Fluorescence)) + 
   geom_point() +
   xlab("") +
   ylab("Chla (µg/L)")
@@ -396,45 +434,42 @@ buoy.plot.del. <- function(df., var., ylab., ylim.) {
     # 2024 and more
     geom_vline(aes(xintercept = last(ts_22to23.raw$Datetime)), col = "blue", alpha = 0.4) + 
     geom_vline(aes(xintercept = first(df.raw$Datetime)), col = "blue", alpha = 0.4) + 
+    geom_vline(data = bluelines, aes(xintercept = Datetime), col = "blue", alpha = 0.4) +
     theme_bw() +
     theme(axis.text.x = element_text(angle = 45, hjust=1))
-  # 2024 and more
-  for(i in 1:length(bluelines.holes)){ 
-    p. <<- p. + geom_vline(aes(xintercept = bluelines.holes[i]), col = "blue", alpha = 0.4)
-  }
-  #return(p.)
 }
 
-y. <- c(min(ts_22to24.raw$Temperature, na.rm=T), max(ts_22to24.raw$Temperature, na.rm=T))
-buoy.plot.del.(df. = ts_22to24.raw, var. = "Temperature", ylab. = "Température (°C)", ylim. = y.)
+
+y. <- c(min(ts_full.raw$Temperature, na.rm=T), max(ts_full.raw$Temperature, na.rm=T))
+buoy.plot.del.(df. = ts_full.raw, var. = "Temperature", ylab. = "Température (°C)", ylim. = y.)
 names(diff.temp.ts)[names(diff.temp.ts) == 'Temperature'] <- "plot.var."
 (p.temp.del. <- p. + geom_point(data=diff.temp.ts,colour="red",size = 0.8))
 rm(p.,y.)
 ggsave("ts_24/Figs/deleted/p.temp.del.png", width = 5, height = 2.5, dpi = 300)
 
-y. <- c(25, max(ts_22to24.raw$Salinite, na.rm=T))
-buoy.plot.del.(df. = ts_22to24.raw, var. = "Salinite", ylab. = "Salinite", ylim. = y.)
+y. <- c(25, max(ts_full.raw$Salinite, na.rm=T))
+buoy.plot.del.(df. = ts_full.raw, var. = "Salinite", ylab. = "Salinite", ylim. = y.)
 names(diff.sal.ts)[names(diff.sal.ts) == 'Salinite'] <- "plot.var."
 (p.sal.del. <- p. + geom_point(data=diff.sal.ts,colour="red",size = 0.8))
 rm(p.,y.)
 ggsave("ts_24/Figs/deleted/p.sal.del.png", width = 5, height = 2.5, dpi = 300)
 
-y. <- c(min(ts_22to24.raw$Oxygene, na.rm=T), max(ts_22to24.raw$Oxygene, na.rm=T))
-buoy.plot.del.(df. = ts_22to24.raw, var. = "Oxygene", ylab. = "Oxygène dissous (mg/L)", ylim. = y.)
+y. <- c(min(ts_full.raw$Oxygene, na.rm=T), max(ts_full.raw$Oxygene, na.rm=T))
+buoy.plot.del.(df. = ts_full.raw, var. = "Oxygene", ylab. = "Oxygène dissous (mg/L)", ylim. = y.)
 names(diff.O2.ts)[names(diff.O2.ts) == 'Oxygene'] <- "plot.var."
 (p.oxy.del. <- p. + geom_point(data=diff.O2.ts,colour="red",size = 0.8))
 rm(p.,y.)
 ggsave("ts_24/Figs/deleted/p.oxy.del.png", width = 5, height = 2.5, dpi = 300)
 
 y. <- c(5,9)
-buoy.plot.del.(df. = ts_22to24.raw, var. = "pH", ylab. = "pH", ylim. = y.)
+buoy.plot.del.(df. = ts_full.raw, var. = "pH", ylab. = "pH", ylim. = y.)
 names(diff.pH.ts)[names(diff.pH.ts) == 'pH'] <- "plot.var."
 (p.pH.del. <- p. + geom_point(data=diff.pH.ts,colour="red",size = 0.8))
 rm(p.,y.)
 ggsave("ts_24/Figs/deleted/p.pH.del.png", width = 5, height = 2.5, dpi = 300)
 
-y. <- c(min(ts_22to24.raw$Fluorescence, na.rm=T), 10)
-buoy.plot.del.(df. = ts_22to24.raw, var. = "Fluorescence", ylab. = "Chla (µg/L)", ylim. = y.)
+y. <- c(min(ts_full.raw$Fluorescence, na.rm=T), 10)
+buoy.plot.del.(df. = ts_full.raw, var. = "Fluorescence", ylab. = "Chla (µg/L)", ylim. = y.)
 names(diff.fluo.ts)[names(diff.fluo.ts) == 'Fluorescence'] <- "plot.var."
 (p.fluo.del. <- p. + geom_point(data=diff.fluo.ts,colour="red",size = 0.8))
 rm(p.,y.)
@@ -442,7 +477,7 @@ ggsave("ts_24/Figs/deleted/p.fluo.del.png", width = 5, height = 2.5, dpi = 300)
 
 # boxplot
 
-m <- melt(ts_22to24.clean[, c(2:6,10)], id.vars= "mois")
+m <- melt(ts_full.clean[, c(2:6,10)], id.vars= "mois")
 m <- m %>% drop_na(mois)
 m$mois <- as.factor(m$mois)
 m$value <- as.numeric(m$value)
---
+++ ts_24/Figs/Readme.txt
@@ -0,0 +1,8 @@
+Dans ce dossier
+
+- Un dossier avec les 4 figs de synthèse présentées lors de notre réunion.
+- Un dossier avec les variables plottées individuellement, données brutes non filtrées.
+- Un dossier avec les variables plottées individuellement, données supprimées des ts finales (points rouges).
+- Un dossier avec les variables plottées individuellement, données nettoyées (plot du pptx).
+
+NB: Les lignes bleues des figs "raw" et "deleted" correspondent aux sorties/entretiens de la sonde, ou journées d'enregistrements aberrants, àpd de l'analyse de Lovina, des dates données par Kévin et des analyses de Jonathan.
